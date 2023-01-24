1251b065f14577dbf1238f5282943fc9991f92f1
Vitaliy Tymchuk <tymchuk94@gmail.com>
2023-01-24 11:50:28 +0000

    add timer function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,58 +5,85 @@ let startButton = document.getElementById("start");
 let questionsArea = document.getElementById("questions");
 let question = document.getElementById("question-title");
 let choiseBox = document.getElementById("choices");
+let timer = document.getElementById("time");
 let questionIndex = 0;
+let timerCounting = 0;
 
 
-// init();
 
 
-startButton.addEventListener("click", function startGame() {
-    questionsArea.classList.remove("hide");
-     startButton.disabled = true;
-    question.textContent = questions[0].question;
 
-    for (let i = 0; i < questions[questionIndex].options.length; i++) {
-        const option = document.createElement("button");
-        option.className = "option";
-        option.textContent = questions[questionIndex].options[i];
-        choiseBox.appendChild(option);
+// startButton.addEventListener("click", function startGame() {
+//     questionsArea.classList.remove("hide");
+//      startButton.disabled = true;
+//     question.textContent = questions[0].question;
 
-        option.addEventListener("click", function (event) {
-        const button1 = event.target;
-        if (button1.textContent === questions[0].rightOption) {
-            console.log("you won");
-            questionIndex++;
-            nextQuestion();
-        };
-    });
+//     for (let i = 0; i < questions[questionIndex].options.length; i++) {
+//         const option = document.createElement("button");
+//         option.className = "option";
+//         option.textContent = questions[questionIndex].options[i];
+//         choiseBox.appendChild(option);
+
+//         option.addEventListener("click", function (event) {
+//          const button1 = event.target;
+//          if (button1.textContent === questions[0].rightOption) {
+//              console.log("you won");
+//              questionIndex++;
+//             nextQuestion();
+//         };
+//     });
    
-};   
-});
+// };   
+// });
 
 
-
-
-function nextQuestion() {
-    question.textContent = questions[questionIndex].question;
-    var buttonList = document.querySelectorAll(".option");
-    // console.log(liList);
+// function nextQuestion() {
+//     question.textContent = questions[questionIndex].question;
+//     var buttonList = document.querySelectorAll(".option");
+//     // console.log(liList);
     
-    for (let i = 0; i < questions[questionIndex].options.length; i++) {
-        buttonList[i].textContent = questions[questionIndex].options[i];  
-    };  
-   
-    option.addEventListener("click", function (event) {
-        const button1 = event.target;
-        if (button1.textContent === questions[0].rightOption) {
-            console.log("you won");
-            questionIndex++;
-            nextQuestion();
-        };
-    });
-};  
+//     for (let i = 0; i < questions[questionIndex].options.length; i++) {
+//         buttonList[i].textContent = questions[questionIndex].options[i];  
+//     };  
+//     chooseOption();
+//     console.log("you won");
+//     questionIndex++;
+//     nextQuestion();
+
+// };
 
 
-    
+
+// function chooseOption(e) {
+//     // choiseBox.addEventListener("click", function (event) {
+//         const button = e.target;
+//         if (button1.textContent === questions[questionIndex].rightOption) {
+//             console.log("you won");
+//             questionIndex++;
+//             nextQuestion();
+//         };
+// // });
+// };
+
+startButton.addEventListener("click", begin);
+
+function countdown() {
+    timerCounting = 60;
+      var timeInterval = setInterval(function () {
+        timerCounting--;
+     timer.textContent = timerCounting;
+    }, 1000);
+  };
+
+
+
+    function begin() {
+        questionsArea.classList.remove("hide");
+        countdown();
+        
+
+    }
+
+
 
 
